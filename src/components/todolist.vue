dde6f9d3cff9de56e9df87d8ac604de5dd77f915
hanna <690433104@qq.com>
2017-03-01 17:34:36 +0800

    add new type

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -4,10 +4,11 @@
 		<div>
 			<div class="btn-group m_t_m" role="group">
 				<button type="button" class="btn btn-default" @click="showAllLists" v-bind:class="{'btn-success':isActive}">全部</button>
-				<!--<button type="button" class="btn btn-default" v-for="(typeList,index) in typeLists" v-bind:class="{'btn-success':isIndex==index?true:false}" @click="showCurrList(index)">{{typeList}}</button>-->
-				<button type="button" class="btn btn-default" @click="showInListType=true"><i class="fa fa-plus"></button>
+				<button type="button" class="btn btn-default" v-for="(typeList,index) in typeLists" v-bind:class="{'btn-success':isIndex==index?true:false}" @click="showCurrList(index)">{{typeList}}</button>
+				<button type="button" class="btn btn-default" @click="showAddTypeModal"><i class="fa fa-plus"></button>
 				<button type="button" class="btn btn-default" @click="manageList"><i class="fa fa-gear"></button>
 			</div>
+			<span class="message">{{msg}}</span>
 		</div>
 		<div v-if="showInListType">
 			<div class="model">
@@ -18,11 +19,11 @@
 							<h4 class="modal-title">为新增的分类取个名字</h4>
 						</div>
 						<div class="modal-body">
-							<input type="text" class="form-control" placeholder="请输入分类的名称" v-model="newItem" @keyup.13="addItem" @focus="checkLogin" @blur="clearMsg">
+							<input type="text" class="form-control" placeholder="请输入分类的名称" v-model="newType" @keyup.13="addNewType">
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-default" @click="showInListType=false">取消</button>
-							<button type="button" class="btn btn-primary" @click="addNewType()">确定</button>
+							<button type="button" class="btn btn-primary" @click="addNewType">确定</button>
 						</div>
 					</div>
 				</div>
@@ -37,7 +38,6 @@
 					<input type="text" class="form-control" placeholder="请添加计划" v-model="newItem" @keyup.13="addItem" @focus="checkLogin" @blur="clearMsg">
 				</div>
 			</div>
-			<p class="message">{{msg}}</p>
 			<div class="task-list">
 				<ul>
 					<li v-for="item in items" :class="{finished:item.isfinished}" v-if="!item.isfinished" @oncontextmenu="showLeft=true">
@@ -86,16 +86,14 @@
 				isActive: true,
 				showFinishedList: false,
 				items: [],
-				//				currType:0,
 				typeLists: [],
 				currIndex: "",
 				isIndex: -1,
 				showInListType: false,
-				showLeft:false
+				showLeft: false,
+				newType: "",
+				mainItems: []
 			};
-		},
-		components: {
-			aside
 		},
 		watch: {
 			allitems: {
@@ -110,12 +108,17 @@
 			this.allitems.forEach(
 				(item) => {
 					if(item.id == store.state.cid) {
-						this.items.push(item);
+						//计划不为空的计划，显示在列表中的计划
+						if(item.plan.trim() != "") {
+							this.items.push(item);
+						}
+						//该用户所有的计划
+						this.mainItems.push(item);						
 					}
 				}
 			);
-			//获取当前登录用户的清单列表中的分类
-			this.items.forEach(
+			//获取当前登录用户的清单列表中的分类（不管该分类是否有内容）
+			this.mainItems.forEach(
 				(item) => {
 					if(this.typeLists.indexOf(item.subtype) < 0) {
 						this.typeLists.push(item.subtype)
@@ -125,13 +128,15 @@
 			console.log("清单：当前是否已登录:" + (store.state.isLogin ? " 是 " : " 否 ") + " ,当前id为 " + store.state.cid)
 		},
 		methods: {
-			addNewType: function() {
-				showInListType = false
-
+			showAddTypeModal: function() {
+				if(!store.state.cid) {
+					this.msg = "请先登录"
+				}
+				this.showInListType = true
 			},
 			manageList: function() {
 				if(!store.state.cid) {
-					alert("请先登录")
+					this.msg = "请先登录"
 					return;
 				}
 				this.$router.push({
@@ -144,10 +149,18 @@
 			//获取当前登录用户的清单列表
 			getCurrItems: function() {
 				this.items = []
+				this.mainItems=[]
 				this.allitems.forEach(
 					(item) => {
 						if(item.id == store.state.cid) {
-							this.items.push(item);
+							//计划不为空的计划，显示在列表中的
+							if(item.plan.trim() != "") {
+								this.items.push(item);
+								console.log(this.items)
+							}
+							//所有的列表
+							this.mainItems.push(item);
+							console.log(this.mainItems)
 						}
 					}
 				);
@@ -155,7 +168,7 @@
 			//获取当前登录用户的清单列表中的分类
 			getTypeLists: function() {
 				this.typeLists = []
-				this.items.forEach(
+				this.mainItems.forEach(
 					(item) => {
 						if(this.typeLists.indexOf(item.subtype) < 0) {
 							this.typeLists.push(item.subtype)
@@ -163,7 +176,7 @@
 					}
 				);
 			},
-			//获取当前登录用户的清单列表中的所有分类
+			//获取当前登录用户的清单列表中的“全部”分类
 			getAllLists: function() {
 				this.getCurrItems()
 			},
@@ -171,9 +184,9 @@
 			getOtherLists: function() {
 				this.getCurrItems()
 				this.items = []
-				this.allitems.forEach(
+				this.mainItems.forEach(
 					(item) => {
-						if(item.subtype == this.currIndex) {
+						if(item.subtype == this.currIndex && item.plan.trim() != ""){
 							this.items.push(item);
 						}
 					}
@@ -182,7 +195,7 @@
 			//点击全部按钮
 			showAllLists: function() {
 				if(!store.state.cid) {
-					alert("请先登录")
+					this.msg = "请先登录"
 					return;
 				}
 				this.isActive = true;
@@ -204,10 +217,6 @@
 			},
 			//切换显示已完成清单
 			toggleFinishedList: function() {
-				if(!store.state.cid) {
-					alert("请先登录")
-					return;
-				}
 				this.showFinishedList = !this.showFinishedList
 			},
 			showDefault: function() {
@@ -231,19 +240,40 @@
 					return;
 				}
 				if(!store.state.cid) {
-					alert("请先登录")
+					this.msg = "请先登录"
 					return;
 				}
 				this.allitems.push({
 					id: store.state.cid,
 					plan: this.newItem,
-					//					subtype: this.isActive ? this.currType : this.currIndex,
+					subtype: this.isActive ? "自定义" : this.newType,
 					isfinished: false
 				})
 				this.getCurrItems();
 				this.getTypeLists();
-				//				this.isActive ? this.getAllLists() : this.getOtherLists();
+				this.isActive ? this.getAllLists() : this.getOtherLists();
 				this.newItem = ""
+			},
+			addNewType: function() {
+				if(!store.state.cid) {
+					this.msg = "请先登录"
+					return;
+				}
+				if(this.newType.trim() == "") {
+					return;
+				}
+				this.allitems.push({
+					id: store.state.cid,
+					plan: "",
+					subtype: this.newType,
+					isfinished: false
+				})
+				this.getCurrItems();
+				this.getTypeLists();
+				this.isActive ? this.getAllLists() : this.getOtherLists();
+				this.newItem = ""
+
+				this.showInListType = false
 			}
 		}
 	}
