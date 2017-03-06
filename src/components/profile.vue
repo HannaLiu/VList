afe56b4223d1e6a5bc8069233801a6669c1b2f42
hanna <690433104@qq.com>
2017-03-06 22:32:34 +0800

    profile（undone）

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,26 +1,64 @@
 ﻿<template>
-	<div id="profile">
-		<h4><center>当前为个人资料页，暂未完成</center></h4>
+	<div id="profile" class="clearfix">
+		<form class="form-horizontal m_t_m col-sm-8 col-sm-push-2 ">
+		  <div class="form-group">
+		    <label class="col-sm-2 control-label">用户名：</label>
+		    <div class="col-sm-10">
+		      <p class="form-control-static">{{name}}</p>
+		    </div>
+		  </div>
+		   <div class="form-group">
+		    <label class="col-sm-2 control-label">手机号：</label>
+		    <div class="col-sm-10">
+		      <input type="tel" class="form-control" id="inputTel" placeholder="电话号码" v-model="tel">		 
+		    </div>
+		  </div>
+		   <div class="form-group">
+		    <label class="col-sm-2 control-label">邮　箱：</label>
+		    <div class="col-sm-10">
+		    	 <input type="email" class="form-control" id="inputEmail" placeholder="邮箱" v-model="email">	
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <label for="inputPassword" class="col-sm-2 control-label">密　码：</label>
+		    <div class="col-sm-10">
+		      <input type="password" class="form-control" id="inputPassword" placeholder="确认密码">
+		    </div>
+		  </div>
+		  <div class="text-center m_t_m"><button type="submit" class="btn btn-success" @click="modify">保存</button></div>
+		</form>
 	</div>
 </template>
 
 <script>
+	import store from '../store/store-global'
 	import storeuser from '../data/storeuser'
-	import md5 from 'md5'
-	import findel from '../modules/findElem'
 	
 	export default {
 		name: 'profile',
 		data() {
 			return {
-				
+				users: storeuser.fetch(),
+				name:store.state.cname,
+				tel:storeuser.tel,
+				email:storeuser.email
 			};
 		},
 		watch: {
-			
+			users: {
+				handler: function(users) {
+					storeuser.save(users)
+				},
+				deep: true
+			}
+		},
+		mounted:function(){
+			console.log(this.users)
 		},
 		methods: {			
-			
+			modify:function(){
+				
+			}
 		}
 		
 	}
